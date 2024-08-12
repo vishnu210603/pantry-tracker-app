3736fd1fa237d81ebc259cfa0bcacd41b7c7b365
vishnu2106 <vishnuyadav2106@gmail.com>
2024-08-12 14:25:29 +0530

    second commit

--- SignIn.js
+++ SignIn.js
@@ -1,74 +1,3 @@
-// // import React, { useContext } from 'react';
-// // import { FcGoogle } from 'react-icons/fc';
-// // import { authContext } from './authcontext';
-
-// // function SignIn() {
-// //   const { googleLoginHandler } = useContext(authContext);
-
-// //   return (
-// //     <main className='flex flex-col min-h-screen justify-center items-center bg-pantry-bg'>
-// //       <div className='flex flex-col items-center gap-10'>
-// //         <div className="text-center text-4xl font-medium bg-white p-4 rounded-lg">
-// //           Welcome to the Pantry-Tracker!
-// //         </div>
-// //         <div className='w-52 h-40 flex flex-col items-center justify-center'>
-// //           <img 
-// //             src="https://rowwad.qa/wp-content/uploads/2023/06/Artboard-2-copy-33.png"
-// //             className='w-20 h-20'
-// //             alt="Logo"
-// //           />
-// //           <button 
-// //             onClick={googleLoginHandler} 
-// //             className='btn flex items-center justify-center gap-2 w-fit h-12 rounded-2xl text-white bg-violet-500 mt-4'
-// //           >
-// //             <FcGoogle className='text-2xl' />
-// //             Sign in with Google
-// //           </button>
-// //         </div>
-// //       </div>
-// //     </main>
-// //   );
-// // }
-
-// // export default SignIn;
-
-
-
-// import React from 'react';
-// import { useContext } from 'react';
-// import { FcGoogle } from 'react-icons/fc';
-// import { authContext } from './authcontext';
-
-// function SignIn() {
-//   const { googleLoginHandler } = useContext(authContext);
-
-//   return (
-//     <main className="flex flex-col min-h-screen justify-center items-center bg-white">
-//       <div className="flex justify-center items-center gap-10 flex-col">
-//         <div className="text-center text-4xl font-medium">Welcome to the Pantry-Tracker!</div>
-//         <div className="flex flex-col items-center">
-//           <img 
-//             src="https://rowwad.qa/wp-content/uploads/2023/06/Artboard-2-copy-33.png"
-//             className="w-20 h-20 mb-4"
-//             alt="Pantry Tracker Logo"
-//           />
-//           <button 
-//             onClick={googleLoginHandler} 
-//             className="flex items-center justify-center gap-2 w-fit h-12 px-4 rounded-2xl text-white bg-violet-500"
-//           >
-//             <FcGoogle className="text-2xl mr-2" />
-//             Sign in with Google
-//           </button>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
-
-// export default SignIn;
-
-
-
 import React from 'react';
 import { useContext } from 'react';
 import { FcGoogle } from 'react-icons/fc';
@@ -80,11 +9,11 @@ function SignIn() {
   return (
     <main className="flex flex-col min-h-screen justify-center items-center bg-white">
       <div className="flex justify-center items-center gap-10 flex-col">
-        <div className="text-center text-4xl font-bold">Welcome to the Pantry-Tracker!</div>
-        <div className="flex flex-col items-center">
+        <div className="text-center text-4xl gap-2 font-bold mt-4">Welcome to the Pantry-Tracker!</div>
+        <div className="flex flex-col  items-center mt-10">
           <img 
-            src="https://rowwad.qa/wp-content/uploads/2023/06/Artboard-2-copy-33.png"
-            className="w-20 h-20 mb-4"
+            src="https://static.wixstatic.com/media/474e97_3adcffeaf219479a9cc4774e57c1838c~mv2.png/v1/fill/w_353,h_318,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/manual-img_bc9570e4.png"
+            className="w-20 h-20"
             alt="Pantry Tracker Logo"
           />
           <button 
